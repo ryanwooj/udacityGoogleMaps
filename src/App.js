3311deade824ff57db9e31ad082557814bd8bd6c
Ryan Woo <woo@Ryans-MacBook-Pro.local>
2018-10-17 03:23:55 -0700

    Separated 2 View Components Map & Search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { GoogleApiWrapper, Map, Marker, InfoWindow } from 'google-maps-react';
+import MapContainer from './MapContainer.js';
+import SearchContainer from './SearchContainer.js'
 
-import './App.css';
-// import WithMarkers from './MapContainer'
 
 class App extends Component {
+
   state = {
-    currentName: null,
+    query: '',
+    items: [],
+    results: [],
+    currentRestaurant: null,
     postition: null,
     selectedPlace: {},
     showingInfoWindow: false,
@@ -17,36 +20,41 @@ class App extends Component {
       {name: "Man Soo Korean BBQ", location: {lat: 34.057969, lng: -118.303994}},
       {name: "YUP DDUK LA", location: {lat: 34.063988, lng: -118.300759}},
     ]
-  };
+  }
 
-  handleToggleOpen(name, item) {
+  filterList = (event) => {
     this.setState({
-      currentName: name,
-      position: {
-        lat: item.lat,
-        lng: item.lng
+      query: event.target.value
+    })
+    var updatedList = this.state.results
+    updatedList = updatedList.filter(item => item.name.toLowerCase().search(event.target.value.toLowerCase()) !== -1 )
+    this.setState({items: updatedList})
+  }
+
+  //
+  // componentWillReceiveProps (props) {
+  //   this.setState({
+  //     items: this.state.results
+  //   })
+  // }
+
+  handleToggleOpen = (loc) => {
+    this.setState({
+      currentRestaurant: {
+        name: loc.name,
+        position: {
+          lat: loc.geometry.location.lat,
+          lng: loc.geometry.location.lng
+        },
+        openingHours: loc.opening_hours.open_now,
+        rating: loc.rating,
+        address: loc.vicinity
       }
     })
   }
-  //
-  // onMarkerClick = (props, marker, e) => {
-  //   console.log(props)
-  // }
-    // this.setState({
-    //   activeMarker: marker,
-    //   selectedPlace: props,
-    //   showingInfoWindow: true
-    // });
-
-  // onMarkerClick = () =>
-  //   this.setState({
-  //     showingInfoWindow: true
-  //   })
-
 
   onInfoWindowClose = () =>
     this.setState({
-      activeMarker: null,
       showingInfoWindow: false
     });
 
@@ -58,46 +66,76 @@ class App extends Component {
       });
   };
 
+  changeMarker = () => {
+
+  }
+
+  createCORSRequest(method, url) {
+     var xhr = new XMLHttpRequest();
+
+     if ("withCredentials" in xhr) {
+
+        // Check if the XMLHttpRequest object has a "withCredentials" property.
+        // "withCredentials" only exists on XMLHTTPRequest2 objects.
+        xhr.open(method, url, true);
+     }else {
+
+        // Otherwise, CORS is not supported by the browser.
+        xhr = null;
+     }
+     return xhr;
+  }
+
+  componentDidMount() {
+    const xhr = this.createCORSRequest('GET', 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=34.0577889,-118.3009088&radius=1500&type=restaurant&keyword=korean&key=AIzaSyBqXhHQkXbDQ6_tKD0Hbd86dfsiJ7ypFS0');
+    xhr.onload = () => {
+      var response = JSON.parse(xhr.responseText)
+      this.setState({
+        results: response.results,
+        locations: response.results,
+        items: response.results
+      })
+    }
+    xhr.onerror = () => {
+      console.log('error')
+    }
+    xhr.send()
+  }
+
+
   render() {
-    if (!this.props.loaded) return <div>Loading...</div>;
-
     return (
-      <div>
-        <Map
-          className="map"
-          google={this.props.google}
-          onClick={this.onMapClicked}
-          style={{ height: '100%', position: 'relative', width: '100%' }}
-          initialCenter={{ lat: 34.06122, lng: -118.301668}}
-          zoom={16}
-        >
-          {this.state.locations.map((loc, index) =>
-            (<Marker
-              name={loc.name}
-              onClick={() => this.handleToggleOpen(loc.name, loc.location)}
-              position={loc.location}
-              animation={this.props.google.maps.Animation.DROP}
-              key={index}>
-
-            </Marker>
-            )
-          )}
-          {this.state.position &&
-            <InfoWindow
-              position={this.state.position} visible>
-              <div>
-                <h2>{this.state.currentName}</h2>
-                <p>This is one of my Favorite Restaurants</p>
-              </div>
-            </InfoWindow>
-          }
-        </Map>
+      <div className="container">
+        <SearchContainer
+          currentRestaurant={this.state.currentRestaurant}
+          position={this.state.position}
+          showingInfoWindow={this.state.showingInfoWindow}
+          locations={this.state.locations}
+          handleToggleOpen={this.handleToggleOpen}
+          onInfoWindowClose={this.onInfoWindowClose}
+          onMapClicked={this.onMapClicked}
+          query={this.state.query}
+          handleInputChange={this.handleInputChange}
+          results={this.state.results}
+          handleSubmit={this.handleSubmit}
+          filterList={this.filterList}
+          items={this.state.items}
+        />
+        <MapContainer
+          currentRestaurant={this.state.currentRestaurant}
+          position={this.state.position}
+          showingInfoWindow={this.state.showingInfoWindow}
+          locations={this.state.locations}
+          handleToggleOpen={this.handleToggleOpen}
+          onInfoWindowClose={this.onInfoWindowClose}
+          onMapClicked={this.onMapClicked}
+          results={this.state.results}
+          items={this.state.items}
+        />
       </div>
 
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyBqXhHQkXbDQ6_tKD0Hbd86dfsiJ7ypFS0'
-})(App)
+export default App;
