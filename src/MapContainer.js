2e60c34c3bce14b2432e8d1bd57f61261f6c0498
Ryan Woo <woo@Ryans-MacBook-Pro.local>
2018-10-17 03:27:11 -0700

    Now Retrieve nearby restaurants from google API and fetch them

---
+++ src/MapContainer.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { GoogleApiWrapper, Map, Marker, InfoWindow } from 'google-maps-react';
+
+import './App.css';
+
+class MapContainer extends Component {
+
+  render() {
+    if (!this.props.loaded) return <div>Loading...</div>;
+
+    const { items, results, currentRestaurant, position, showingInfoWindow, locations, handleToggleOpen, OnInfoWindowClose, onMapClicked } = this.props
+
+    return (
+      <div className="app">
+        <Map
+          className="map"
+          google={this.props.google}
+          onClick={onMapClicked}
+          style={{ height: '100%', position: 'relative', width: '100%' }}
+          initialCenter={{ lat: 34.06000, lng: -118.293392}}
+          zoom={15}
+        >
+          {items.map((loc, index) =>
+            (<Marker
+              name={loc.name}
+              onClick={() => handleToggleOpen(loc)}
+              position={loc.geometry.location}
+              animation={this.props.google.maps.Animation.DROP}
+              key={loc.id}>
+
+            </Marker>
+            )
+          )}
+          {currentRestaurant &&
+            <InfoWindow
+              position={currentRestaurant.position} visible>
+              <div>
+                <h2 className="res-name">{currentRestaurant.name}</h2>
+                {currentRestaurant.openingHours? (<p className="open">Now Open!</p>): (<p className="closed">Currently Closed</p>)}
+
+                <p className="res-rating">Rating: {currentRestaurant.rating}</p>
+                <p className="res-address">Address: {currentRestaurant.address} </p>
+              </div>
+            </InfoWindow>
+          }
+        </Map>
+      </div>
+
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBqXhHQkXbDQ6_tKD0Hbd86dfsiJ7ypFS0'
+})(MapContainer)
---
+++ src/SearchContainer.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class SearchContainer extends Component {
+  state = {
+    query: '',
+    items: this.props.results,
+  }
+
+  render() {
+    const { items, query, filterList, handleSubmit, changeMarker, results, handleInputChange,  currentRestaurant, position, showingInfoWindow, locations, handleToggleOpen, onInfoWindowClose, onMapClicked } = this.props
+
+    return (
+      <div className="sidebar">
+        <div className="menu" tabIndex="0">
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
+        </div>
+        <div className="list-view">
+          <h1>Ryan's Favorite Restaurants</h1>
+          <input
+            type="text"
+            value={query}
+            placeholder="Search in List..."
+            onChange={filterList}
+            role="search"
+            aria-label="Search Ryans'"
+          />
+          <ul id="list">
+            {items.map((item, index) => (
+              <li key={index}>
+                <button className="button">
+                  {item.name}
+                </button>
+              </li>
+            ))}
+            {/* {locations ? (
+              locations.map((place, index) => {
+                return (
+              <li key={index}>
+              <button
+              className="button"
+              // onClick={this.openWindow}
+              >{place.name}
+              </button>
+              </li>
+                )
+              })
+              ): (
+              <li className="loading">Loading</li>
+            )} */}
+          </ul>
+        </div>
+      </div>
+
+
+    );
+  }
+
+}
+
+export default SearchContainer;
